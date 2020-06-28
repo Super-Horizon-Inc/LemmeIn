87cc8da6ae0d9cc8680786acdc004cf1ede76025
Dang Nguyen <nguyenminhdang1995@gmail.com>
2020-06-27 22:16:44 -0500

    setup customer controller to manage customer repositories

---
+++ server/src/main/java/com/super_horizon/lemmein/models/repositories/CustomerRepository.java
@@ -0,0 +1,16 @@
+package com.super_horizon.lemmein.models.repositories;
+
+import java.util.List;
+
+import com.super_horizon.lemmein.models.documents.Customer;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends MongoRepository<Customer, String> {
+
+    List<Customer> findByPhoneNumber(String phoneNumber);
+
+    List<Customer> findByEmail(String email);
+
+}
