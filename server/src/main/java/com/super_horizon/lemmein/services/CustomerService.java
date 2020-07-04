ea149b29014b5deb2180dfc1a285c2ff97adb5a4
dangnguyenn <44311172+dangnguyenn@users.noreply.github.com>
2020-07-04 14:00:57 -0500

    Added end point for searchOrAdd, searchById, searchAll, edit  (#8)
    * initial commit
    
    Signed-off-by: Dang Nguyen <nguyenminhdang1995@gmail.com>
    
    * added server
    
    * a

---
+++ server/src/main/java/com/super_horizon/lemmein/services/CustomerService.java
@@ -0,0 +1,69 @@
+package com.super_horizon.lemmein.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+import com.super_horizon.lemmein.models.repositories.CustomerRepository;
+import com.super_horizon.lemmein.models.documents.*;
+import java.util.*;
+
+@Service
+public class CustomerService {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    public List<Customer> searchOrAdd(Map<String, String> query) {
+
+        try {
+            List<Customer> customers = customerRepository.findOrCreate(query);
+            return customers;
+        }
+        catch (Exception ex) {
+            return new ArrayList<Customer>();
+        }        
+    }
+
+    public Customer searchById(String id) {
+
+        Customer _customer = customerRepository.findById(id).get();
+
+        if (!Objects.isNull(_customer)) {
+            return _customer;
+        }
+
+        throw new NullPointerException();       
+    }
+
+    public Customer edit(String id, Customer customer) {
+
+        Customer _customer = this.searchById(id);
+
+        if (!"".equals(customer.getPhoneNumber()) ) {
+            _customer.setPhoneNumber(customer.getPhoneNumber());
+        }
+        else {
+            _customer.setEmail(customer.getEmail());
+        }
+
+        _customer.setDOB(customer.getDOB());
+        _customer.setFirstName(customer.getFirstName());
+        _customer.setLastName(customer.getLastName());
+
+        customerRepository.save(_customer);
+
+        return _customer;
+    }
+
+    public List<Customer> searchAll() {
+
+        List<Customer> customers = customerRepository.findAll();
+        
+        if (!customers.isEmpty()) {
+            return customers;
+        }
+
+        throw new NullPointerException();
+    }
+
+}
