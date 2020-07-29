5d8f4256dee94250c4a8da88575dbc78667524b3
dangnguyenn <44311172+dangnguyenn@users.noreply.github.com>
2020-07-28 22:27:46 -0500

    Added user login and logout (#24)
    * initial commit
    
    Signed-off-by: Dang Nguyen <nguyenminhdang1995@gmail.com>
    
    * added server
    
    * added user login and logout

---
+++ services/AuthService.js
@@ -0,0 +1,68 @@
+import { AsyncStorage } from 'react-native';
+
+export default class AuthService {
+
+    constructor(username, password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    getCurrentUsername = async () => {
+        return await AsyncStorage.getItem('username');
+    }
+    
+
+    signin = () => {
+        const result = fetch("https://31df3a354fd7.ngrok.io/lemme/user/auth/signin", {
+            method: 'POST',
+            headers: {
+                Accept : 'application/json',
+                'Content-Type' : 'application/json'              
+            },
+            body: JSON.stringify({username: this.username, password: this.password})
+            })
+            .then(response => 
+                response.json()
+            )
+            .then(json => {
+
+                if (json.accessToken != null) {
+                    AsyncStorage.setItem('user', json.accessToken);     
+                    AsyncStorage.setItem('username', this.username);
+                }
+                return json;
+                
+            })
+            .catch(error => {                   
+                console.error(error);
+            });
+
+        return result;
+    }
+
+
+    logout = async (input) => {
+        return await fetch("https://31df3a354fd7.ngrok.io/lemme/user/auth/logout", {
+            method: 'POST',
+            headers: {
+                Accept : 'application/json',
+                'Content-Type' : 'application/json',
+            },
+            body: JSON.stringify(input)
+            })
+            .then(response => 
+                response.json()
+            )
+            .then( json => {
+                if (json.message.indexOf('Logout successfully.') >= 0) {
+                    AsyncStorage.removeItem('user');
+                    AsyncStorage.removeItem('username');   
+                }            
+                return json.message; 
+            })
+            .catch(error => {                   
+                console.error(error);
+            });
+    }
+
+}
